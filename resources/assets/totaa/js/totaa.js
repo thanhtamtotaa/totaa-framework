0126957ccbfac96d1b7c709d613edb8cd996b800
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-23 11:30:30 +0700

    first push commit

---
+++ resources/assets/totaa/js/totaa.js
@@ -0,0 +1,69 @@
+var isRtl = $("html").attr("dir") === "rtl";
+
+function Customize_BlockUI() {
+    $.blockUI({
+        message:
+            '<div class="sk-fold sk-primary mx-auto mb-4"><div class="sk-fold-cube"></div><div class="sk-fold-cube"></div><div class="sk-fold-cube"></div><div class="sk-fold-cube"></div></div><h5 class="text-primary">Đang xử lý...</h5>',
+        css: {
+            backgroundColor: "transparent",
+            border: "0",
+            zIndex: 9999999
+        },
+        overlayCSS: {
+            backgroundColor: "#fff",
+            opacity: 0.6,
+            zIndex: 9999990
+        },
+        centerX: true,
+        centerY: true,
+        onBlock: function() {
+            $("div.blockUI.blockMsg.blockPage").css(
+                "top",
+                "calc(50% - " +
+                    $("div.blockUI.blockMsg.blockPage").height() / 2 +
+                    "px )"
+            );
+
+            $("div.blockUI.blockMsg.blockPage").css(
+                "left",
+                "calc(50% - " +
+                    $("div.blockUI.blockMsg.blockPage").width() / 2 +
+                    "px )"
+            );
+        }
+    });
+}
+
+function Upload_File_Customize_BlockUI() {
+    $.blockUI({
+        message:
+            '<div class="bg-white p-5"><div class="sk-fold sk-primary mx-auto mb-4"><div class="sk-fold-cube"></div><div class="sk-fold-cube"></div><div class="sk-fold-cube"></div><div class="sk-fold-cube"></div></div><div id="TT_blockUI_custom" class="my-3"></div><h5 class="text-primary">Đang xử lý...</h5></div>',
+        css: {
+            backgroundColor: "transparent",
+            border: "0",
+            zIndex: 9999999
+        },
+        overlayCSS: {
+            backgroundColor: "#fff",
+            opacity: 0.6,
+            zIndex: 9999990
+        },
+        centerX: true,
+        centerY: true,
+        onBlock: function() {
+            $("div.blockUI.blockMsg.blockPage").css(
+                "top",
+                "calc(50% - " +
+                    $("div.blockUI.blockMsg.blockPage").height() / 2 +
+                    "px )"
+            );
+
+            $("div.blockUI.blockMsg.blockPage").css(
+                "left",
+                "calc(50% - " +
+                    $("div.blockUI.blockMsg.blockPage").width() / 2 +
+                    "px )"
+            );
+        }
+    });
+}
