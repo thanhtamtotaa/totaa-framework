a43e1d7874c045bc226fe6823b32ebd99617d8bc
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-26 14:44:54 +0700

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HttpsProtocolMiddleware::class,
+            \App\Http\Middleware\CheckBrowser::class,
         ],
 
         'api' => [
@@ -64,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'CheckBrowser' => \App\Http\Middleware\CheckBrowser::class,
+        'CheckAccount' => \App\Http\Middleware\CheckAccount::class,
     ];
 }
