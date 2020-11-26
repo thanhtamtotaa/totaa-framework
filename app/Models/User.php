3b5809ecc245210d7ddd147deee06aa953ff17af
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-11-26 14:45:47 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Totaa\TotaaBfo\Traits\HasBfoInfos;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
     use SoftDeletes;
+    use HasBfoInfos;
 
     /**
      * The attributes that are mass assignable.
